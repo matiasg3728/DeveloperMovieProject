519819c645902f94ec17d2166d3d2a269ae999ab
Matias Garcia <matiasg3728@gmail.com>
2019-03-10 03:07:00 -0500

    I changed the Title of the home page to show "Movies R' Us" (layout file).
    I also created the data model files for both the Genre and Movie structures.

---
+++ MovieProj_Matias/MovieProj_Matias/Models/Genre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieProj_Matias.Models
+{
+    public class Genre
+    {
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Movie> Movies { get; set; }
+
+    }
+}
---
+++ MovieProj_Matias/MovieProj_Matias/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace MovieProj_Matias.Models
+{
+    public class Movie
+    {
+
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public int year { get; set; }
+
+        public ICollection<Genre> Genre { get; set; }
+
+
+    }
+}
