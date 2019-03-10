4216d1dd999d8a264a197c4c4e448bfc39862575
Matias Garcia <matiasg3728@gmail.com>
2019-03-10 01:16:58 -0600

    please work

---
+++ MovieProj_Matias/MovieProj_Matias/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MovieProj_Matias.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
