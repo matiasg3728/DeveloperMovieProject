ca73aeff3050a5e7893833a402632e3a0bcb6a7c
Matias Garcia <matiasg3728@gmail.com>
2019-03-16 12:08:44 -0500

    yeeeeeeeeeet

--- MovieProj_Matias/MovieProj_Matias/Startup.cs
+++ MovieProj_Matias/MovieProj_Matias/Startup.cs
@@ -63,7 +63,7 @@ namespace MovieProj_Matias
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Movies}/{action=Index}/{id?}");
             });
         }
     }
