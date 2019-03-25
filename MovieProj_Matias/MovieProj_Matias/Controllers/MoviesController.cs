d233d347d7473752593dc3b56d60ac0881041cae
Matias Garcia <matiasg3728@gmail.com>
2019-03-24 23:10:13 -0500

    We have done quite a bit. - Created a ViewComponent, Genre Selector - Began styling the home page in order to create the navigation bar ------
    Next up we will be working on adding in jQuery and Ajax in order to update the table that displays movies on the home page.

--- MovieProj_Matias/MovieProj_Matias/Controllers/MoviesController.cs
+++ MovieProj_Matias/MovieProj_Matias/Controllers/MoviesController.cs
@@ -25,6 +25,16 @@ namespace MovieProj_Matias.Controllers
             return View(await _context.Movies.ToListAsync());
         }
 
+
+        /*
+            GET: Movies of a certain genre         
+         */
+        public async Task<IActionResult> MovieGroup(int? genreID) {
+
+          
+            return View();
+        }
+
         // GET: Movies/Details/5
         public async Task<IActionResult> Details(int? id)
         {
---
+++ MovieProj_Matias/MovieProj_Matias/ViewComponents/GenreSelector.cs
@@ -0,0 +1,32 @@
+﻿using MovieProj_Matias.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MovieProj_Matias.Models;
+
+namespace MovieProj_Matias.ViewComponents
+{
+    public class GenreSelectorViewComponent : ViewComponent
+    { 
+        private readonly CinimaContext db;
+
+
+        public GenreSelectorViewComponent(CinimaContext context)
+        {
+            db = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int? selectedGenre) {
+
+            var genres = await GetGenres();
+
+            return View(genres);
+        }
+
+        private Task<List<Genre>> GetGenres() {
+            return db.Genres.ToListAsync();
+        }
+    }
+}
